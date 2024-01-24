48e3e255ae268b3ed17d4368e63dde83d850782d
GitHElias <jorge.elias@potros.itson.edu.mx>
2024-01-24 15:35:50 -0700

    Add Cliente fields

--- CRUDmySQL/src/main/java/com/mycompany/crudmysql/Cliente.java
+++ CRUDmySQL/src/main/java/com/mycompany/crudmysql/Cliente.java
@@ -3,11 +3,64 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.crudmysql;
+import java.time.*;
 
 /**
  *
  * @author Ryzen 5
  */
-public class Cliente {
+public class Cliente 
+{
+    int idCliente;
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    Boolean isDeleted;
+    LocalDateTime fechaHoraRegistro;
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public LocalDateTime getFechaHoraRegistro() {
+        return fechaHoraRegistro;
+    }
+
+    public void setFechaHoraRegistro(LocalDateTime fechaHoraRegistro) {
+        this.fechaHoraRegistro = fechaHoraRegistro;
+    }
+    
     
 }
