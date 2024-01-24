f18efb9306af01927bfd456b6c0ac73787e171f1
GitHElias <47585501+GitHElias@users.noreply.github.com>
2024-01-24 12:44:00 -0700

    Created project and added dependencies

---
+++ CRUDmySQL/src/main/java/com/mycompany/crudmysql/CRUDmySQL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crudmysql;
+
+/**
+ *
+ * @author Ryzen 5
+ */
+public class CRUDmySQL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
